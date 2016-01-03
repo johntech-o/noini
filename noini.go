d3b718c15f97161d9ba657dc8c3a812a19c832eb
johntech-o <zhoukunta@qq.com>
2016-01-03 17:37:05 +0800

    add some new files

--- noini.go
+++ noini.go
@@ -1,35 +1,29 @@
+// 入口文件
 package noini
 
-type Uri string
-
-type Cmd struct {
-	u Uri
-	k Key
-	v interface{}
+type ini struct {
+	s *Storage
+	r *Router
 }
 
-type iniStorage struct {
-	version  uint
-	recvBuf  chan *Cmd
-	files    map[Uri]*file
-	Sections map[Uri]*Section
-}
-
-func (i *ini) GetSectionsByUri(uri []string) *SectionBook {
-
-}
-
-func (i *ini) NextCmd() *Cmd {
-	select {
-	case c, ok := <-i.recvBuf:
-		if !ok {
-			break
-		}
-		return c, nil
+func New(path string) (*ini, error) {
+	storage := NewStorage()
+	err := storage.ParsePath(path)
+	if err != nil {
+		return nil, err
 	}
-	return nil, errors.New("close cmd channel")
+	router = NewRouter(storage)
+	return &ini{s: storage, r: router}
+	return i, nil
 }
 
-func Parse(path string) *ini {
+// register your own uri router otherwise use default route parser
+func (i *ini) RegisterRouter(parser ParseFunc) {
+	r.parser = parser
+}
 
+// 订阅指定uri，按优先级传入，会返回订阅的 Session
+// 用户订阅的uri是一个Session，会根据默认的Router来解析订阅规则
+func (i *ini) SubByUri(uri []string) (*Session, error) {
+	return i.r.Parse(subs)
 }
--- parser.go
+++ parser.go
@@ -1,56 +1,97 @@
 package noini
 
-type ValueType string
+import (
+	"bufio"
+	"io"
+	"strconv"
+)
 
 type Parser struct {
-	modeValueType      bool
-	modeValueTypeCheck int
-	checkMap           map[FileName]map[KeyName]ValueType
+	modeValueType bool // if true parse as typed ini
+	fs            *FilesSet
+	lines         []string
+	comment       []int
 }
+
+type Uri string
+
+type ValueType string
 
 type KeyName string
 
+// section key
 type Key struct {
-	name    KeyName
-	lineNum int
-	comment []string
-	section *Section
+	name      KeyName
+	lineNum   int
+	comment   []string
+	valuetype ValueType
+	value     string
+	belongTo  *Section
 }
 
-type Value struct {
-	typ     ValueType
-	section *Section
-}
-
-type SectionBook struct {
-	mergeSection  *Section
-	sectionMap    map[Uri]*Section
-	sortedSection []*Section
-	root          *ini
-}
-
-func (s *SectionBook) WriteCmd(cmd) {
-
-}
-
+// section in file
 type Section struct {
-	uri        string
-	parent     *Section
+	uri        Uri
 	name       string
-	key        Key
-	value      Value
+	keys       map[KeyName]*Key
 	sum        string
-	priority   int
 	lineAmount int
 	file       *File
 }
 
-type FileName string
+// parse file
+func (p *Parser) ParseSection(reader io.Reader, section *Section) error {
+	stream := bufio.NewReader(reader)
+	lineNum = 0
+	var comment []string
+	for {
+		line, err := stream.ReadString('\n')
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		line = strings.TrimSpace(line)
+		p.lines = append(p.lines, line)
+		length := len(line)
+		switch {
+		case l[0] == '#': // parse comment
+			comment = append(comment, lineNum-1)
+			goto next
+		case l[0] == '[' && l[length-1] == ']': // New section.
+			goto next
+		default:
+			pos := strings.IndexAny(line, "=")
+			if pos <= 0 {
+				return nil, fmt.Errorf("ParseSection error: line num: %d ", strconv.Itoa(pos))
+			}
+			// parse the key
+			left := strings.TrimSpace(line[0:pos])
+			fields := strings.Fields(left)
+			key := Key{name: left, lineNum: lineNum, comment: comment, belongTo: &section}
+			// parse the value
+			var vt ValueType = "string"
+			if len(fields) == 2 {
+				vt = strings.TrimSpace(fields[1])
+			}
+			key.valuetype = vt
+			key.value = strings.TrimSpace(text[pos+1:])
+			section[key.name] = &key
+			key.comment = comment
+			comment = comment[0:0]
+			goto next
+		}
+	next:
+		lineNum++
+		if err == io.EOF {
+			section.lineAmount = lineNum + 1
+			return nil, section
+		}
+	}
+}
 
-type File struct {
-	uri      Uri
-	name     FileName
-	ext      string
-	sections map[Uri]*Section
-	lines    []byte
+// 一组相关的file集合（比如文件名相同的），递归扫描root目录下所有同名文件,并且组织成同一个集合,用于类型检测
+// 类型检测递归扫描路径，发现冲突记录第一个冲突位置和文件
+type FilesSet struct {
+	fileName      FileName
+	valueTypeBook map[KeyName]ValueType // valueType of all key in deferent file
+	firstConflict map[Uri]KeyName       // file Uri and conflicted KeyName
 }
